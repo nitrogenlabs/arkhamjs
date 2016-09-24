52c5fbd23ffb881f7ffaaf81f36472138a254ff0
Giraldo Rosales <giraldo@nitrogenlabs.com>
2016-09-24 17:33:58 -0500

    updated index

--- index.js
+++ index.js
@@ -10,5 +10,4 @@ exports.__esModule = true;
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 exports.Flux = _interopRequireDefault(require('./lib/Flux'))['default'];
-exports.FluxNative = _interopRequireDefault(require('./lib/FluxNative'))['default'];
 exports.Store = _interopRequireDefault(require('./lib/Store'))['default'];
