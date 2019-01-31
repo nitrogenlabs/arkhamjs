acb4b0907a28629b2ef824d0da2851d9d61557e9
Giraldo Rosales <giraldo@nitrogenlabs.com>
2019-01-30 21:18:15 -0600

    Update packages

--- packages/arkhamjs/src/Flux/Flux.test.ts
+++ packages/arkhamjs/src/Flux/Flux.test.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2018-Present, Nitrogen Labs, Inc.
  * Copyrights licensed under the MIT License. See the accompanying LICENSE file for terms.
  */
-import {set} from 'lodash';
+import set from 'lodash/set';
 
 import {Store} from '../Store/Store';
 import {FluxAction, FluxOptions} from '../types/flux';
