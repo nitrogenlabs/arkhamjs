dcc344bf1c7e1d92379ad385bad9e5a1214568d4
Giraldo Rosales <giraldo@nitrogenlabs.com>
2019-01-30 21:18:15 -0600

    Update packages

--- packages/arkhamjs/src/Store/Store.test.ts
+++ packages/arkhamjs/src/Store/Store.test.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2018-Present, Nitrogen Labs, Inc.
  * Copyrights licensed under the MIT License. See the accompanying LICENSE file for terms.
  */
-import {isObject} from 'lodash';
+import isObject from 'lodash/isObject';
 
 import {Store} from './Store';
 
