fe05638a21230d338c344fd76bdce27edbea567f
Giraldo Rosales <giraldo@nitrogenlabs.com>
2019-11-07 20:42:43 -0600

    Cleanup node storage

--- packages/arkhamjs-storage-node/src/index.ts
+++ packages/arkhamjs-storage-node/src/index.ts
@@ -2,9 +2,7 @@
  * Copyright (c) 2018-Present, Nitrogen Labs, Inc.
  * Copyrights licensed under the MIT License. See the accompanying LICENSE file for terms.
  */
-
-// Storage
 import {NodeStorage} from './NodeStorage/NodeStorage';
 
-export * from './types/main';
+// Storage
 export {NodeStorage};
