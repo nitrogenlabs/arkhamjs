4eda37fdcce3c708f6df6b1940c54db50b73bb52
Giraldo Rosales <giraldo@nitrogenlabs.com>
2020-07-29 15:12:17 -0700

    Fix example unit tests

--- packages/arkhamjs-example-ts-react/src/services/StringService.test.ts
+++ packages/arkhamjs-example-ts-react/src/services/StringService.test.ts
@@ -1,11 +1,11 @@
-import {StringService} from './StringService';
+import {capitalize, uppercaseWords} from './StringService';
 
 describe('StringService', () => {
   describe('.uppercaseWords', () => {
     it('should uppercase words', () => {
       const str = 'test string';
       const expected = 'Test String';
-      return expect(StringService.uppercaseWords(str)).toBe(expected);
+      return expect(uppercaseWords(str)).toBe(expected);
     });
   });
 
@@ -13,7 +13,7 @@ describe('StringService', () => {
     it('should capitalize word', () => {
       const str = 'test';
       const expected = 'Test';
-      return expect(StringService.capitalize(str)).toBe(expected);
+      return expect(capitalize(str)).toBe(expected);
     });
   });
 });
