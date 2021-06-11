0678556096d54873da4bc9559c2bf95f3e5d9d3b
Giraldo Rosales <giraldo@nitrogenlabs.com>
2021-06-10 23:04:47 -0500

    Re-render when state is updated

--- packages/arkhamjs-utils-react/src/useFluxState.ts
+++ packages/arkhamjs-utils-react/src/useFluxState.ts
@@ -2,11 +2,18 @@
  * Copyright (c) 2020-Present, Nitrogen Labs, Inc.
  * Copyrights licensed under the MIT License. See the accompanying LICENSE file for terms.
  */
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 import {FluxContext} from './FluxContext';
 
 export const useFluxState = (key: string | string[], defaultValue?: any): any => {
   const {flux} = useContext(FluxContext);
-  return flux.getState(key, defaultValue);
+  const value = flux.getState(key, defaultValue);
+  const [state, setState] = useState(value);
+
+  useEffect(() => {
+    setState(value);
+  }, [value]);
+
+  return state;
 };
