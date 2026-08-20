cd43ec0790cf08a760c5792e140bf7151b0d23c0
Giraldo Rosales <giraldo@nitrogenx.co>
2026-08-20 08:06:26 -0500

    fix: exporting static types

---
+++ packages/arkhamjs-devtools-extension/src/index.ts
@@ -0,0 +1,5 @@
+export {InspectorActions} from './actions/index.js';
+export {TabBar} from './components/index.js';
+export {InspectorConstants} from './constants/index.js';
+export type {InspectorDispatchType, InspectorStackType} from './types/inspector.js';
+export {ActionsView, InfoView, LayoutView, StateView} from './views/index.js';
